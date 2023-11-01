1f4add90b61de9772c1734e7f66d8236b287e2f7
Shivam02032000 <95584915+Shivam02032000@users.noreply.github.com>
2023-11-01 10:43:11 +0530

    Create 03_vardemo.sh

---
+++ 03_vardemo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Script to show how to use variables
+
+a=10
+name="Shivam"
+age=23
+
+echo "My name is $name and age is $age"
+
+
+#Var to store the output of a command
+HOSTNAME=$(hostname)
+echo "Name of this machine is $HOSTNAME"
