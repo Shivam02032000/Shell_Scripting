85ac4896abc0e19dff778966276c294b14c7db3e
Shivam02032000 <95584915+Shivam02032000@users.noreply.github.com>
2024-01-05 20:02:12 +0530

    Create 08_user_int.sh

---
+++ 08_user_int.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Taking input from user
+#1st way
+
+echo "What is your name?"
+
+read name
+
+echo "You name is $name"
+
+
+
+#Another way
+
+read -p "What is your full name? " fullName
+
+echo "Your name is $fullName"
+
+----------------------------------------------------------------------------------------------------------------
+
+Output:-
+
+What is your name?
+Shivam
+You name is Shivam
+What is your full name? Shivam Chandra
+Your name is Shivam Chandra
+
+
